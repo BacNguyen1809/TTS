127de0b0114bbee090b440c2348ed1463ceb4b78
BacNguyen <bacnguyen1005@gmail.com>
2023-12-17 18:18:24 +0700

    up

--- setup-cuda.sh
+++ setup-cuda.sh
@@ -7,9 +7,8 @@ python3 -m venv venv
 source ./venv/bin/activate
 python3 -m pip install --upgrade pip # just to be safe
 # CUDA
-# pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-# CPU
-pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cpu
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+
 # install requirements
 python3 -m pip install -r ./modules/tortoise-tts/requirements.txt # install TorToiSe requirements
 python3 -m pip install -e ./modules/tortoise-tts/ # install TorToiSe
